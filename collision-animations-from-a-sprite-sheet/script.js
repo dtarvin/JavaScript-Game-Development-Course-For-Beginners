e292a3052859eedb883e846a1d93a63e3773ee35
david.tarvin <david.tarvin@physiciansmutual.com>
2023-09-23 14:55:04 -0500

    part of collisions from a sprite sheet

---
+++ collision-animations-from-a-sprite-sheet/script.js
@@ -0,0 +1,43 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = 500;
+canvas.height = 700;
+const explosions = [];
+let canvasPosition = canvas.getBoundingClientRect();
+
+class Explosion {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.spriteWidth = 200;
+        this.spriteHeight = 179;
+        this.width = this.spriteWidth / 2;
+        this.height = this.spriteHeight / 2;
+        this.image = new Image();
+        this.image.src = './boom.png';
+        this.frame = 0;
+    }
+    update() {
+        this.frame++;
+    }
+    draw() {
+        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight,
+                        this.x, this.y, this.width, this.height);
+    }
+}
+
+window.addEventListener('click', function (e) {
+    let positionX = e.x - canvasPosition.left;
+    let positionY = e.x - canvasPosition.top;
+    explosions.push(new Explosion(positionX, positionY));
+});
+
+function animate() {
+    for (let i = 0; i < explosions.length; i++) {
+        explosions[i].update();
+        explosions[i].draw();
+    }
+    requestAnimationFrame(animate);
+}
+animate();
